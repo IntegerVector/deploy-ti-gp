580642440fee50a265f6548ad6f60e1701ff0ddc
IntegerVector <andrii.ruban@outlook.com>
2022-10-10 20:17:26 +0300

    script update

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,22 @@
 
 if [ -z $1 ]
 then
-    echo "You MUST specify target branch!"
+    echo "You MUST specify repo!"
     exit 1
 fi
 if [ -z $2 ]
+then
+    echo "You MUST specify target branch!"
+    exit 1
+fi
+if [ -z $3 ]
 then
     echo "You MUST specify version"
     exit 1
 fi
 
-echo "Deploying to GitHub pages, to branch $1"
-echo "Build version $2"
+echo "Deploying to GitHub pages $1, to branch $2"
+echo "Build version $3"
 
 set -e
 # Change next 2 lines according to your application scripts!
@@ -23,8 +28,8 @@ cd dist
 git init
 git branch -M main
 git add -A
-git commit -m "'deploy v$2'"
-git tag "$2"
-git push -f git@github.com:IntegerVector/flash-cards.git main:$1
-git push git@github.com:IntegerVector/flash-cards.git --tags
+git commit -m "'deploy v$3'"
+git tag "$3"
+git push -f $1 main:$2
+git push $1 --tags
 cd -
